8891d88c930973945f3752e46d134178fd17b329
Neil Griffin <neil.griffin.scm@gmail.com>
2019-03-21 15:18:42 -0400

    MVCS-22 Work-around incompatibility with JstlView.useInclude(HttpServletRequest,HttpServletResponse) and Apache Pluto

--- framework/src/main/java/com/liferay/portletmvc4spring/PortletJstlView.java
+++ framework/src/main/java/com/liferay/portletmvc4spring/PortletJstlView.java
@@ -15,7 +15,9 @@
  */
 package com.liferay.portletmvc4spring;
 
+import javax.portlet.ResourceRequest;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.MessageSource;
 
@@ -25,8 +27,13 @@ import com.liferay.portletmvc4spring.util.PortletContainer;
 
 
 /**
- * This class provides a work-around for a JSTL incompatibility with Apache Pluto. See the {@link
- * #exposeHelpers(HttpServletRequest)}} method for details.
+ * This class provides work-arounds for two incompatibilities with Apache Pluto:
+ *
+ * <ol>
+ *   <li>JSTL incompatibility -- see the {@link #exposeHelpers(HttpServletRequest)}} method for details.</li>
+ *   <li>Request dispatcher incompatibility -- see the {@link #useInclude(HttpServletRequest, HttpServletResponse)}
+ *     method for more details</li>
+ * </ol>
  *
  * @author  Neil Griffin
  * @since   5.1.0
@@ -46,5 +53,27 @@ public class PortletJstlView extends JstlView {
 		if (!PortletContainer.PLUTO.isDetected(request)) {
 			super.exposeHelpers(request);
 		}
+	}
+
+	/**
+	 * If the specified request is associated with Apache Pluto and the RESOURCE_PHASE of the portlet lifecycle, then
+	 * this method returns <code>true</code> in order to force PortletMVC4Spring to dispatch using "include" rather than
+	 * "forward". This is because Apache Pluto is not able to render JSPs using the RESOURCE_PHASE using a "forward" --
+	 * it can only do it with an "include". For all other portlet containers and types of requests, this method simply
+	 * calls through to the superclass method.
+	 */
+	@Override
+	protected boolean useInclude(HttpServletRequest request, HttpServletResponse response) {
+
+		if (PortletContainer.PLUTO.isDetected(request)) {
+
+			Object portletRequest = request.getAttribute("javax.portlet.request");
+
+			if (portletRequest instanceof ResourceRequest) {
+				return true;
+			}
+		}
+
+		return super.useInclude(request, response);
 	}
 }
