5479aa22edfd9ac2f6393322a298a910ad5752ad
Neil Griffin <neil.griffin.scm@gmail.com>
2021-06-01 20:33:42 -0400

    MVCS-50 Upgrade to Spring Framework 5.3.7 and Spring Security 5.5.0 (workaround UnsupportedOperationException thrown by org.apache.pluto.portlet.servlet.adapter.HttpServletRequestAdapter.getDispatcherType() when used with Spring Framework 5.3.x)

---
+++ security/src/main/java/com/liferay/portletmvc4spring/security/SpringSecurityRequestAdapter.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.portletmvc4spring.security;
+
+import javax.portlet.PortletRequest;
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletContext;
+
+import org.apache.pluto.portlet.servlet.adapter.HttpServletRequestAdapter;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class SpringSecurityRequestAdapter extends HttpServletRequestAdapter {
+
+	private DispatcherType dispatcherType;
+
+	public SpringSecurityRequestAdapter(PortletRequest portletRequest, DispatcherType dispatcherType) {
+		super(portletRequest);
+		this.dispatcherType = dispatcherType;
+	}
+
+	public SpringSecurityRequestAdapter(PortletRequest portletRequest, ServletContext servletContext,
+										DispatcherType dispatcherType) {
+		super(portletRequest, servletContext);
+		this.dispatcherType = dispatcherType;
+	}
+
+	@Override
+	public DispatcherType getDispatcherType() {
+		return dispatcherType;
+	}
+}
